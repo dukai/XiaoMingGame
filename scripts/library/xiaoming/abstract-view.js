6c94b8b94f48f55f3760d7b1cda98c71427c28cb
DK <dukai86@gmail.com>
2013-09-12 17:36:33 +0800

    添加view抽象类

---
+++ scripts/library/xiaoming/abstract-view.js
@@ -0,0 +1,11 @@
+define(function(require, exports, module){
+	var AbstractView = function(options){
+		this._initAbstractView(options);
+	};
+	
+	AbstractView.prototype = {
+		_initAbstractView: function(options){
+			
+		}
+	};
+});
