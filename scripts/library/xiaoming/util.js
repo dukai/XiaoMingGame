3e3b3bf7a5188775d8295b94b88dd248e2c5968d
DK <dukai86@gmail.com>
2013-09-13 16:52:58 +0800

    修改util工具类

--- scripts/library/xiaoming/util.js
+++ scripts/library/xiaoming/util.js
@@ -32,7 +32,7 @@ define(function(require, exports, module){
 			left: scrollx,
 			top: scrolly
 		};
-	}
+	};
 	util.getScroll = getScroll;
 		
 	var getPosInDoc = function(target) {
@@ -63,7 +63,7 @@ define(function(require, exports, module){
 			try{ //Firefox, Mozilla, Opera, etc.
 				xmlDoc=document.implementation.createDocument("","",null);
 			}catch(e) {
-				console.log(e.message)
+				console.log(e.message);
 			}
 		}
 
@@ -72,7 +72,7 @@ define(function(require, exports, module){
 			xmlDoc.load(docName);
 			return(xmlDoc);
 		}catch(e) {
-			console.log(e.message)
+			console.log(e.message);
 		}
 
 		return(null);
@@ -200,12 +200,12 @@ define(function(require, exports, module){
 		var context = canvas.getContext('2d');
 		
 		return {canvas: canvas, context: context};
-	}
+	};
 	util.createCanvas = createCanvas;
 	
 	var rand = function(m, n) {
 		return Math.floor((n - m + 1) * Math.random() + m);
-	}
+	};
 	
 	util.rand = rand;
 	
@@ -233,159 +233,32 @@ define(function(require, exports, module){
 		var familyName = getRandFamilyName();
 		var firstName = getRandFirstName(gender);
 		return familyName + firstName;
-	}
+	};
 	util.getName = getName;
 	
 	var addGetter = function(construct, attr){
 		var method = 'get' + attr[0].toUpperCase() + attr.slice(1);
 		construct.prototype[method] = function(){
 			return this.attrs[attr];
-		}
-	}
+		};
+	};
 	util.addGetter = addGetter;
 	
 	var addSetter = function(construct, attr){
 		var method = 'set' + attr[0].toUpperCase() + attr.slice(1);
 		construct.prototype[method] = function(value){
 			return this.attrs[attr] = value;
-		}
-	}
+		};
+	};
 	util.addSetter = addSetter;
 	
 	var addGetterSetter = function(construct, attr){
 		addSetter(construct, attr);
 		addGetter(construct, attr);
-	}
+	};
 	
 	util.addGetterSetter = addGetterSetter;
 	var win = window;
-	var Request = function(options){
-		var self = this;
-		self.requestHeaders = {'DK_AJAX_REQUEST': 'ajax-reqeust'};
-		self.options = {url: '', success: function(){}, error: function(){}, method: 'GET'};
-		for(var i in options){
-			self.options[i] = options[i];
-		}
-		
-		if(self.options.dataType == 'jsonp'){
-		
-			var jsonpCallbackName = 'dkjsonp_' + new Date().getTime() + parseInt(Math.random() * 1000 )
-			self.options.url = self.options.url.replace('callback=?', 'callback=' + jsonpCallbackName);
-			
-			win[jsonpCallbackName] = win[jsonpCallbackName] || function(data){
-				self.options.success(data);
-				
-				win[ jsonpCallbackName ] = undefined;
-				
-				try{
-					delete win[jsonpCallbackName];
-				}catch(e){}
-			};
-			
-			var head = document.getElementsByTagName('head')[0] || document.documentElement;
-			var script = document.createElement("script");
-			script.src = self.options.url;
-			var done = false;
-	
-			// Attach handlers for all browsers
-			script.onload = script.onreadystatechange = function() {
-				if ( !done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") ) {
-					done = true;
-	
-					// Handle memory leak in IE
-					script.onload = script.onreadystatechange = null;
-					if ( head && script.parentNode ) {
-						head.removeChild( script );
-					}
-				}
-			};
-			
-			head.insertBefore( script, head.firstChild );
-			
-			self.send = function(){};
-			return;
-		}
-		
-		var xhr = null;
-		if(win.XMLHttpRequest){
-			xhr = new XMLHttpRequest();
-		}else{
-			xhr = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		
-		xhr.onreadystatechange = function(){
-			switch(xhr.readyState){
-				case 1:
-					self.loading();
-					break;
-				case 2:
-					self.loaded();
-					break;
-				case 3:
-					self.interactive();
-					break;
-				case 4:
-					self.complete(xhr.status, xhr.statusText, xhr.responseText, xhr.responseXML);
-					break;
-			}
-		};
-		
-		if(xhr.upload && self.options.progress){
-			xhr.upload.addEventListener('progress', self.options.progress, false);
-		}
-		
-		self.send = function(data){
-			xhr.open(self.options.method, self.options.url, true);
-			for(var i in self.requestHeaders){
-				xhr.setRequestHeader(i, self.requestHeaders[i]);
-			}
-			data = data ? data : null;
-			xhr.send(data);
-		};
-		
-		self.setRequestHeader = function(label, content){
-			self.requestHeaders[label] = content;
-		};
-		
-		self.loading = function(){};
-		self.loaded = function(){};
-		self.interactive = function(){};
-		self.complete = function(status, statusText, responseText, responseXML){
-			if(status == 200){
-				if(self.options.success){
-					var responseObj;
-					if(self.options.dataType){
-						switch(self.options.dataType){
-							case 'json':
-								responseObj = util.JSON.parse(responseText);
-								break;
-							case 'html':
-								responseObj = responseText;
-								break;
-							case 'xml':
-								if(responseXML){
-									responseObj = responseXML;
-								}else{
-									responseObj = responseText;
-								}
-								break;
-						}
-					}else{
-						if(responseXML){
-							responseObj = responseXML;
-						}else{
-							responseObj = responseText;
-						}
-					}
-					self.options.success.call(self, responseObj, statusText, xhr);
-				}
-			}else{
-				self.options.error && self.options.error(statusText, xhr);
-			}
-		};
-	};
-	
-	util.Request = Request;
 	util.JSON = window.JSON;
 	module.exports = util;
 });
