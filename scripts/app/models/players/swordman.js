bfa2e1590f1110ef169d673cd10f48d54bf1c78c
DK <dukai86@gmail.com>
2013-09-16 17:48:13 +0800

    update model and component

---
+++ scripts/app/models/players/swordman.js
@@ -0,0 +1,13 @@
+define(function(require, exports, module){
+	
+	
+	var Swordman = function(options){
+		this._initSwordman(options);
+	};
+	
+	Swordman.prototype = {
+		_initSwordman: function(options){
+			
+		}
+	};
+});
