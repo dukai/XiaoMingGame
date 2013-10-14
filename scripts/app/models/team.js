81fe45f090ec7b7f58f44945ca16f08fd09b9d80
DK <dukai86@gmail.com>
2013-10-14 16:31:22 +0800

    为team添加getHitMap方法

--- scripts/app/models/team.js
+++ scripts/app/models/team.js
@@ -1,7 +1,7 @@
 define(function(require, exports, module){
     var oo = require('xiaoming/oo');
     var Util = require('xiaoming/util');
-
+	var HitMap = require('xiaoming/map/hit-map');
     var CharEvent = require('app/models/chars/char-event');
 
     var Team =function(options){
@@ -11,16 +11,16 @@ define(function(require, exports, module){
     Team.prototype = {
         _initTeam: function(options){
             this.options = oo.mix({
-
+	            map: []
             }, this.options);
             this.options = oo.mix(this.options, options);
-
             this.chars = [];
             this.charsHashMap = {};
         },
 
         add: function(char){
             if(Util.indexOf(this.chars, char) < 0){
+	            char.team = this;
                 this.chars.push(char);
                 this.charsHashMap[char.getHashCode()] = char;
                 char.eventManager.addEventListener(CharEvent.COORDINATE_CHANGE, this.onCoordinateChange, this);
@@ -39,7 +39,18 @@ define(function(require, exports, module){
         onCoordinateChange: function(event){
             delete this.charsHashMap[Util.pos2HashCode(event.ocx, event.ocy)];
             this.charsHashMap[event.target.getHashCode()] = event.target;
-        }
+        },
+
+	    getHitMap:function(){
+		    var mapCopy = this.options.map.clone();
+		    var hitMap = new HitMap(mapCopy);
+		    for(var key in this.charsHashMap){
+			    var coordinate = Util.hashCode2Pos(key);
+			    hitMap.set(coordinate.x, coordinate.y, 1);
+		    }
+
+		    return hitMap;
+	    }
     };
 
     module.exports = Team;
