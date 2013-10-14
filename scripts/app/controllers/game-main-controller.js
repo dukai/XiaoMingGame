d298d7d65cf444e88ae88919e2040f18f58c262e
DK <dukai86@gmail.com>
2013-10-14 14:47:14 +0800

    修改角色组织方式，添加队伍

--- scripts/app/controllers/game-main-controller.js
+++ scripts/app/controllers/game-main-controller.js
@@ -11,7 +11,8 @@ define(function(require, exports, module){
     var CharEvent = require('app/models/chars/char-event');
     var CharStatus = require('app/models/chars/char-status');
     var TmxMapParser = require('xiaoming/tmx-map-parser');
-	
+	var Team = require('app/models/team');
+
 	var GameMainController = function(options){
 		this._initGameMainController(options);
 	};
@@ -23,14 +24,18 @@ define(function(require, exports, module){
             var tmxMapParser = new TmxMapParser({
                 mapData: resourceLoader.get('v2_map')
             });
+
+			this.ourTeam = new Team();
+			this.enemyTeam = new Team();
+
             this.player1 = CharFactory.createCharacter(CharType.roleType.swordman);
             this.player1.setCoordinate(16, 8);
             this.player2 = CharFactory.createCharacter(CharType.roleType.swordman);
             this.player2.setCoordinate(17, 9);
             this.player2.idColor = CharType.idColorType.red;
-            this.gameModel.addChar(this.player1);
-            this.gameModel.addChar(this.player2);
-            this.gameModel.addEnemies(this.player2);
+
+			this.ourTeam.add(this.player1);
+			this.enemyTeam.add(this.player2);
 
             this.addViewData({
                 tmxMapParser: tmxMapParser
@@ -53,8 +58,8 @@ define(function(require, exports, module){
             });
             this.get('view').layer.add(image);
             */
-            for(var i = 0, len = this.gameModel.chars.length; i< len; i++){
-                var char = this.gameModel.chars[i];
+            for(var i = 0, len = this.ourTeam.chars.length; i< len; i++){
+                var char = this.ourTeam.chars[i];
                 var ctpPlayer = CPTCharFactory.createCharacter(char.charType);
                 this.get('view').layer.add(ctpPlayer);
                 ctpPlayer.changeIdColor(char.idColor);
@@ -73,6 +78,16 @@ define(function(require, exports, module){
 	            char.eventManager.addEventListener(CharEvent.HIDE_MENU, this.get('view').onHideMenu, this.get('view'));
             }
 
+
+			for(var i = 0, len = this.enemyTeam.chars.length; i< len; i++){
+				var char = this.enemyTeam.chars[i];
+				var ctpPlayer = CPTCharFactory.createCharacter(char.charType);
+				this.get('view').layer.add(ctpPlayer);
+				ctpPlayer.changeIdColor(char.idColor);
+				ctpPlayer.start();
+				ctpPlayer.setCoordinate(char.cx, char.cy);
+				char.eventManager.addEventListener(CharEvent.COORDINATE_CHANGE, ctpPlayer.onCoordinateChange, ctpPlayer);
+			}
 		},
 
         onLayerClick: function(e){
@@ -80,8 +95,8 @@ define(function(require, exports, module){
             //如果不存在activedChar判断点击位置
             if(!this.gameModel.activedChar){
                 var hashKey = Util.pos2HashCode(e.coordinate.x, e.coordinate.y);
-                if(this.gameModel.charsHashMap[hashKey] && this.gameModel.charsHashMap[hashKey].status != CharStatus.WAITING){
-                    this.gameModel.activedChar = this.gameModel.charsHashMap[hashKey];
+                if(this.ourTeam.charsHashMap[hashKey]){
+                    this.gameModel.activedChar = this.ourTeam.charsHashMap[hashKey];
                 }else{
                     //如果点击为空白位置，退出
                     return;
