593d05bd7d3e6b7a04813b459ec01aa740629555
DK <dukai86@gmail.com>
2013-10-24 22:36:49 +0800

    更新弓箭手组件和模型

--- scripts/app/components/chars/archer.js
+++ scripts/app/components/chars/archer.js
@@ -3,6 +3,7 @@ define(function(require, exports, module){
 	var oo = require('xiaoming/oo');
 	var AbstractChar = require('./abstract-char');
 	var resourceLoader = require('xiaoming/resource-loader');
+    var CharType = require('app/models/chars/char-type');
 
 	var Archer = function(options){
 		this._initArcher(options);
@@ -13,7 +14,7 @@ define(function(require, exports, module){
 			this.options = oo.mix({
 				image: resourceLoader.get('archer'),
 				darkImage: resourceLoader.get('archer_dark'),
-				fixX: -8,
+				fixX: -0,
 				fixY: -16,
 				defaultAnimation: 'idle',
 				frameRate: 8,
@@ -28,8 +29,7 @@ define(function(require, exports, module){
 						{x: 0, y: 144, width: 36, height: 48},
 						{x: 0, y: 192, width: 36, height: 48},
 						{x: 0, y: 240, width: 36, height: 48},
-						{x: 0, y: 288, width: 36, height: 48},
-						{x: 0, y: 336, width: 36, height: 48}
+						{x: 0, y: 288, width: 36, height: 48}
 					],
 					atk: [
 						{x: 36, y: 0, width:56, height:44},
@@ -53,8 +53,38 @@ define(function(require, exports, module){
 				}
 			}, this.options);
 			AbstractChar.call(this, options);
-		}
+		},
 
+        changeIdColor: function(idColorType){
+            switch (idColorType){
+                case CharType.idColorType.blue:
+                    this.body.setImage(resourceLoader.get('archer'));
+                    this.setImage(resourceLoader.get('archer'));
+                    this.setDarkImage(resourceLoader.get('archer_dark'));
+                    break;
+                case CharType.idColorType.red:
+                    this.body.setImage(resourceLoader.get('archer_red'));
+                    this.setImage(resourceLoader.get('archer_red'));
+                    this.setDarkImage(resourceLoader.get('archer_red_dark'));
+                    break;
+                default :
+                    this.body.setImage(resourceLoader.get('solider'));
+                    break;
+            }
+
+        },
+
+        flip: function(value){
+            if(value == 'left'){
+                this.setScale(1, 1);
+                this.setOffset(0, 0);
+            }
+
+            if(value == 'right'){
+                this.setScale(-1, 1);
+                this.setOffset(32, 0);
+            }
+        },
 	};
 
     Kinetic.Util.extend(Archer, AbstractChar);
--- scripts/app/models/chars/archer.js
+++ scripts/app/models/chars/archer.js
@@ -3,6 +3,7 @@ define(function(require, exports, module){
 	var oo = require('xiaoming/oo');
 	var Equipment = require('../equipments/equipment');
     var CharType = require('./char-type');
+
 	var Archer = function(options){
 		this._initArcher(options);
 	};
@@ -11,24 +12,24 @@ define(function(require, exports, module){
 		_initArcher: function(options){
 			this.options = oo.mix({
 				iPropertiesData: {
-					hitPoint : 80,
-					attackPower : 50,
-					physicalArmor : 25,
+					hitPoint : 60,
+					attackPower : 60,
+					physicalArmor : 20,
 					magicArmor : 10,
 					dodge : .08,
-					criticalStrike : .2,
-					lucky : .3,
-					block : .3,
-					mobility : 4,
-					attackRange : {min: 1, max: 1}
+					criticalStrike : .1,
+					lucky : .1,
+					block : .1,
+					mobility : 3,
+					attackRange : {min: 1, max: 3}
 				}
 			},this.options);
 			PlayerModel.call(this, options);
 
 			//设置装备类型
-			this.armorType = Equipment.armorType.chain;
-			this.weaponType = Equipment.weaponType.sword;
-			this.units = '剑士';
+			this.armorType = Equipment.armorType.leather;
+			this.weaponType = Equipment.weaponType.bow;
+			this.units = '弓箭手';
             this.charType = CharType.roleType.archer;
 		}
 	};
